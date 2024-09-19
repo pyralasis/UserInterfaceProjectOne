ce2c71f4546ff6fe097953347b886a965afc01c0
quazlee <quazlee29@gmail.com>
2024-09-19 11:09:58 -0400

    a

---
+++ src/lib/GameCarousel.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<div id="main-container">
+
+</div>
+
+<style>
+    #main-container{
+        display: flex;
+        background-color: lightslategray;
+        height: 100px;
+        width: 1000px;
+    }
+</style>
